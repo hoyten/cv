d545faae73fea7c971848d3a3ae823a7b7ddbf05
Анастасия <167225420+hoyten@users.noreply.github.com>
2024-12-22 14:17:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: " https://hoyten.github.io/hoyten/cv/assets",
+  base: " https://hoyten.github.io/hoyten/cv",
 })
